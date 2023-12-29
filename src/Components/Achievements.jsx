e5ebde174cdd486d535352c37045804786ab0f52
chetansaini <chetansaini1241@gmail.com>
2023-12-30 03:03:23 +0530

    Achievements Added

--- src/Components/Achievements.jsx
+++ src/Components/Achievements.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React from "react";
+import { RiLinkM } from "react-icons/ri";
 
 function Achievements() {
   return (
-    <div>
+    <div className="achievements">
       <h1 className="text-center text-6xl">Achievements</h1>
-      <div>
-        
+      <div className="lst-items">
+        <ul className="lst-points">
+          <li>
+            Two times ICPC Regionalist : Chennai & Amritapuri.
+          </li>
+          <li>Codeforces : Expert (1773) rated and solved 1100+ problems.<a className="ml-2" href="https://codeforces.com/profile/chetan_saini" target="_blank"><RiLinkM /></a></li>
+          <li>Codechef : 5 star (2061) rated and solved 300+ problems.<a className="ml-2" href="https://www.codechef.com/users/chetan_saini21" target="_blank"><RiLinkM /></a></li>
+          <li>Leetcode : Guardian (2367) rated and solved 300+ problems.<a className="ml-2" href="https://leetcode.com/chetan_saini21/" target="_blank"><RiLinkM /></a></li> 
+          <li> Secured Rank : </li>
+          <ul>
+            <li>1460 in Google Kickstart.</li>
+            <li>172 in Codeforces Round 913 (Div. 3).<a className="ml-2" href="https://codeforces.com/contest/1907/standings/participant/169155237#p169155237" target="_blank"><RiLinkM /></a></li>
+            <li>184 in Codeforces Round 855 (Div. 3).<a className="ml-2" href="https://codeforces.com/contest/1800/standings/participant/150650563#p150650563" target="_blank"><RiLinkM /></a></li>
+            <li>223 in Codeforces Round 866 (Div. 2).<a className="ml-2" href="https://codeforces.com/contest/1820/standings/participant/153857862#p153857862" target="_blank"><RiLinkM /></a></li>
+            <li>350 in Codeforces Round 827 (Div. 4).<a className="ml-2" href="https://codeforces.com/contest/1742/standings/participant/141839175#p141839175" target="_blank"><RiLinkM /></a></li>
+            <li>132 in Leetcode Weekly Contest 369.<a className="ml-2" href="https://leetcode.com/contest/weekly-contest-369/ranking/6/" target="_blank"><RiLinkM /></a></li>
+            <li>132 in Leetcode Biweekly Contest 111.<a className="ml-2" href="https://leetcode.com/contest/biweekly-contest-111/ranking/6/" target="_blank"><RiLinkM /></a></li>
+            <li>26 in Codechef Starters 83 (Div 2).<a className="ml-2" href="https://www.codechef.com/rankings/START83B?itemsPerPage=100&order=asc&page=1&sortBy=rank" target="_blank"><RiLinkM /></a></li>
+            <li>265 in ICPC India Preliminary 2023 Contest.<a className="ml-2" href="https://codedrills.io/contests/icpc-india-preliminary-2023/scoreboard" target="_blank"><RiLinkM /></a></li>
+          </ul>
+          <li>
+            Ranked among top 2.6 percentile students in JEE Main 2021, among 1.2 million students.
+          </li>
+          <li>Finalist in Code Gladiators organised by TechGig.<a className="ml-2" href="https://www.techgig.com/codegladiators/top-ranker" target="_blank"><RiLinkM /></a></li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Achievements
+export default Achievements;
+{
+  /* • Codeforces : Expert (1773) rated and solved 1100+ problems.
+• Codechef : 5 star (2062) rated and solved 300+ problems.
+• Leetcode : Guardian (2291) rated and solved 300+ problems.
+• Secured Rank-
+• 1460 in Google Kickstart.
+• 184 in Codeforces Round 855 (Div. 3).
+• 223 in Codeforces Round 866 (Div. 2).
+• 351 in Codeforces Round 885 (Div. 2).
+• 193 in Leetcode Biweekly Contest 101.
+• 26 in Codechef Starters 83 (Div 2).
+• 340 in ICPC Kanpur-Mathura Site Preliminary Contest.
+• Ranked among top 2.6 percentile students in JEE Main 2021, among 1.2 million students.
+• Finalist in Code Gladiators organised by TechGig. */
+}
