5c0d4b6874a4fd5d4cfe4f086f053bdfc1dd2469
chetansaini <chetansaini1241@gmail.com>
2023-12-30 14:59:07 +0530

    Achievements Improved

--- src/Components/tmp.jsx
+++ src/Components/tmp.jsx
@@ -15,15 +15,15 @@ function Achievements() {
           <li><span className="text-slate-950">Leet</span><span className="text-yellow-600">Code</span> : Guardian&nbsp;<span className="number">(2367)</span>&nbsp;rated and solved&nbsp;<span className="number">300+</span>&nbsp;problems.<a className="ml-2" href="https://leetCode.com/chetan_saini21/" target="_blank"><RiLinkM /></a></li> 
           <li> Secured Rank : </li>
           <ul>
-            <li><span className="number">265</span>&nbsp;in&nbsp;<span className="text-yellow-400">I</span><span className="text-blue-400">CP</span><span className="text-red-400">C</span>&nbsp;India Preliminary&nbsp;<span className="number">2023</span>&nbsp;Contest.<a className="ml-2" href="https://Codedrills.io/contests/icpc-india-preliminary-2023/scoreboard" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">265</span>&nbsp;in&nbsp;<span className="text-yellow-400">I</span><span className="text-blue-400">CP</span><span className="text-red-400">C</span>&nbsp;India Preliminary 2023 Contest.<a className="ml-2" href="https://Codedrills.io/contests/icpc-india-preliminary-2023/scoreboard" target="_blank"><RiLinkM /></a></li>
             <li><span className="number">1460</span>&nbsp;in&nbsp;<span className="text-blue-300">G</span><span className="text-red-400">o</span><span className="text-yellow-400">o</span><span className="text-blue-300">g</span><span className="text-green-400">l</span><span className="text-red-400">e</span>&nbsp;Kickstart.</li>
-            <li><span className="number">172</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">913</span>&nbsp;(Div.&nbsp;<span className="number">3</span>).<a className="ml-2" href="https://Codeforces.com/contest/1907/standings/participant/169155237#p169155237" target="_blank"><RiLinkM /></a></li>
-            <li><span className="number">184</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">855</span>&nbsp;(Div.&nbsp;<span className="number">3</span>).<a className="ml-2" href="https://Codeforces.com/contest/1800/standings/participant/150650563#p150650563" target="_blank"><RiLinkM /></a></li>
-            <li><span className="number">223</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">866</span>&nbsp;(Div.&nbsp;<span className="number">2</span>).<a className="ml-2" href="https://Codeforces.com/contest/1820/standings/participant/153857862#p153857862" target="_blank"><RiLinkM /></a></li>
-            <li><span className="number">350</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">827</span>&nbsp;(Div.&nbsp;<span className="number">4</span>).<a className="ml-2" href="https://Codeforces.com/contest/1742/standings/participant/141839175#p141839175" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">172</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">913</span>&nbsp;(Div. 3).<a className="ml-2" href="https://Codeforces.com/contest/1907/standings/participant/169155237#p169155237" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">184</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">855</span>&nbsp;(Div. 3).<a className="ml-2" href="https://Codeforces.com/contest/1800/standings/participant/150650563#p150650563" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">223</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">866</span>&nbsp;(Div. 2).<a className="ml-2" href="https://Codeforces.com/contest/1820/standings/participant/153857862#p153857862" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">350</span>&nbsp;in&nbsp;<span className="text-yellow-400">Cod</span><span className="text-blue-400">efor</span><span className="text-red-400">ces</span>&nbsp;Round&nbsp;<span className="number">827</span>&nbsp;(Div. 4).<a className="ml-2" href="https://Codeforces.com/contest/1742/standings/participant/141839175#p141839175" target="_blank"><RiLinkM /></a></li>
             <li><span className="number">132</span>&nbsp;in&nbsp;<span className="text-slate-950">Leet</span><span className="text-yellow-600">Code</span>&nbsp;Weekly Contest&nbsp;<span className="number">369</span>&nbsp;.<a className="ml-2" href="https://leetCode.com/contest/weekly-contest-369/ranking/6/" target="_blank"><RiLinkM /></a></li>
             <li><span className="number">132</span>&nbsp;in&nbsp;<span className="text-slate-950">Leet</span><span className="text-yellow-600">Code</span>&nbsp;Biweekly Contest&nbsp;<span className="number">111</span>&nbsp;.<a className="ml-2" href="https://leetCode.com/contest/biweekly-contest-111/ranking/6/" target="_blank"><RiLinkM /></a></li>
-            <li><span className="number">26</span>&nbsp;in&nbsp;<span className="text-gray-100">Code</span><span className="text-yellow-800">Chef</span>&nbsp;Starters&nbsp;<span className="number">83</span>&nbsp;(Div&nbsp;<span className="number">2</span>).<a className="ml-2" href="https://www.CodeChef.com/rankings/START83B?itemsPerPage=100&order=asc&page=1&sortBy=rank" target="_blank"><RiLinkM /></a></li>
+            <li><span className="number">26</span>&nbsp;in&nbsp;<span className="text-gray-100">Code</span><span className="text-yellow-800">Chef</span>&nbsp;Starters&nbsp;<span className="number">83</span>&nbsp;(Div 2).<a className="ml-2" href="https://www.CodeChef.com/rankings/START83B?itemsPerPage=100&order=asc&page=1&sortBy=rank" target="_blank"><RiLinkM /></a></li>
           </ul>
           <li>Qualified for&nbsp;<span className="text-blue-500">Flipkart</span>&nbsp;Grid SDE track&nbsp;<span className="number">1.2</span>.</li>
           <li>Ranked among top&nbsp;<span className="number">2.6</span>&nbsp;percentile students in JEE Main&nbsp;<span className="number">2021</span>, among&nbsp;<span className="number">1.2</span>&nbsp;million students.</li>
@@ -35,3 +35,18 @@ function Achievements() {
 }
 
 export default Achievements;
+{
+  /* • Codeforces : Expert (1773) rated and solved 1100+ problems.
+• CodeChef : 5 star (2062) rated and solved 300+ problems.
+• LeetCode : Guardian (2291) rated and solved 300+ problems.
+• Secured Rank-
+• 1460 in Google Kickstart.
+• 184 in Codeforces Round 855 (Div. 3).
+• 223 in Codeforces Round 866 (Div. 2).
+• 351 in Codeforces Round 885 (Div. 2).
+• 193 in LeetCode Biweekly Contest 101.
+• 26 in CodeChef Starters 83 (Div 2).
+• 340 in ICPC Kanpur-Mathura Site Preliminary Contest.
+• Ranked among top 2.6 percentile students in JEE Main 2021, among 1.2 million students.
+• Finalist in Code Gladiators organised by TechGig. */
+}
